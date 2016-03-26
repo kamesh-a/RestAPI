ab9fee29568daed3fe0810526424b77fba42cbe7
Kamesh <kamesh.arumugam@a-cti.com>
2016-03-26 18:46:57 +0530

    added deps and some view changes

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,11 @@ router.get('/', function(req, res, next) {
   		message : '[post] to "http://localhost:3000/getAccess" with {name : "admin" , pass : "admin"} to get Access token',
   		api : 'All Private API need access token to do operation',
   		getAccess: 'POST[public] : "http://localhost:3000/getAccess" with {name : "admin" , pass : "admin"}',
-  		post: 'POST[public] : "http://localhost:3000/:victimClient" will store in DB, returns 201 & location header',
-  		getAll: 'GET[private] : "http://localhost:3000" returns JSON of all clients in DB',
-  		get: 'GET[private] : "http://localhost:3000/:victimClient" returns victimClient',
-  		put: 'PUT[private] : "http://localhost:3000/:victimClient" update long&lat for editing purpose, returns 201 & location header',
-  		del: 'DELETE[private] : "http://localhost:3000/:victimClient" deletes victimClient and returns 204',
+  		post: 'POST[public][ADD] : "http://localhost:3000/location/:victimClient" ( body : {lat & long} ) will store in DB, returns 201 & location header',
+  		getAll: 'GET[private][RETRIVE] : "http://localhost:3000/location" returns JSON of all clients in DB',
+  		get: 'GET[private][RETRIVE] : "http://localhost:3000/location/:victimClient" returns victimClient',
+  		put: 'PUT[private][UPDATE] : "http://localhost:3000/location/:victimClient" ( body : {lat & long} ) update long&lat for editing purpose, returns 201 & location header',
+  		del: 'DELETE[private][DEL] : "http://localhost:3000/location/:victimClient" deletes victimClient and returns 204',
   		token :"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiYWRtaW4iLCJwYXNzd29yZCI6ImFkbWluIiwiaWF0IjoxNDU4OTE5MDM5LCJleHAiOjE0NTg5MjI2Mzl9.ORZmUH47HPjTKh4R1BmyvakHAjTDuaDzkpLR9CxjwBs"
   	});
 });
